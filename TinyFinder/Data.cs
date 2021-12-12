a591d54bc593db426e6f7f14373b2cc9d74abe89
Bambo-Rambo <65967984+Bambo-Rambo@users.noreply.github.com>
2021-12-12 23:13:54 +0200

    Horde Locations

--- TinyFinder/Data.cs
+++ TinyFinder/Data.cs
@@ -57,33 +57,60 @@ namespace TinyFinder
         public static object[,] ORAS_Locations = new object[,]
         {
             //Name, NPC Influence, Has Hordes/Long Grass, Ratio
-            { "Elsewhere", 0, false, 13 },
-            { "Mt Pyre Inside", 0, false, 7 },
-            { "New Mauville", 0, false, 7 },
-            { "Route 111 Desert", 1, false, 1 },    //Ratio is 7 but only 1 works every time
-            { "Route 117", 1, false, 13 },
-            { "Route 118", 0, true, 13 },
-            { "Route 119", 0, true, 13 },
-            { "Route 120", 0, true, 13 },
-            { "Route 121", 0, true, 13 },
-            { "Route 123", 1, true, 13 },
-            { "Safari Zone", 0, true, 13 },
-            { "Sky Pillar", 0, false, 7 },
+            { "Elsewhere", 0, false, 13, },
+            { "Mt Pyre Inside", 0, false, 7, },
+            { "New Mauville", 0, false, 7, },
+            { "Route 111 Desert", 1, false, 1, },    //Ratio is 7 but only 1 works every time
+            { "Route 117", 1, false, 13, },
+            { "Route 118", 0, true, 13, },
+            { "Route 119", 0, true, 13, },
+            { "Route 120", 0, true, 13, },
+            { "Route 121", 0, true, 13, },
+            { "Route 123", 1, true, 13, },
+            { "Safari Zone", 0, true, 13, },
+            { "Sky Pillar", 0, false, 7, },
             //Honey Wild Locations
             //Name, Bag/Honey Advances, null, null
-            { "Elsewhere", 15, null, null },
-            { "Magma/Aqua Hideout", 6, null, null },
-            { "Underwater", 3, null, null },
+            { "Elsewhere", 15, null, null, },
+            { "Magma/Aqua Hideout", 6, null, null, },
+            { "Underwater", 3, null, null, },
+        };
+
+        public static object[,] Horde_Locations = new object[,]
+        {
+            //Name, Bag Advances
+            { "Elsewhere", 27, }, //XY
+            { "Reflection Cave", 6, },
+            { "Route 20", 3, },
+            { "Elsewhere", 15 }, //ORAS
+            { "Mt Pyre Inside", 3, },
+            { "New Mauville", 3, },
+            { "Route 114", 16, },
+            { "Route 121", 16, },
+            { "Rusturf Tunnel", 15, },
+            { "Sky Pillar", 3, },
         };
 
 
-        public List<Location> SetLocations(byte method, bool oras)
+        public List<Location> SetLocations(byte method, bool oras, bool hordeTurn)
         {
             Location area;
             List<Location> Areas = new List<Location>();
             if (oras)
             {
-                if (method == 1 || method == 4)
+                if (method == 4 && !hordeTurn)
+                {
+                    for (byte i = 3; i < 10; i++)
+                    {
+                        area = new Location
+                        {
+                            Name = string.Copy(Horde_Locations[i, 0].ToString()),
+                            Bag_Advances = Convert.ToByte(Horde_Locations[i, 1])
+                        };
+                        Areas.Add(area);
+                    }
+                }
+                else if (method == 1 || method == 4)
                     for (byte i = 0; i < 11; i++)
                     {
                         area = new Location
@@ -96,7 +123,7 @@ namespace TinyFinder
                         if (method == 1 || Equals(area.Name, "Elsewhere") || Equals(area.Name, "Route 123"))
                             Areas.Add(area);
                     }
-                else //if (method == 5)
+                else if (method == 5)
                 {
                     for (byte i = 12; i < 15; i++)
                     {
@@ -111,18 +138,33 @@ namespace TinyFinder
             }
             else
             {
-                for (byte i = 0; i < 23; i++)
+                if (method == 4 && !hordeTurn)
                 {
-                    area = new Location
+                    for (byte i = 0; i < 3; i++)
                     {
-                        Name = string.Copy(XY_Locations[i, 0].ToString()),
-                        NPC = Convert.ToByte(XY_Locations[i, 1]),
-                        Has_Hordes = (bool)XY_Locations[i, 2],
-                        ratio = Convert.ToByte(XY_Locations[i, 3]),
-                        Tall_Grass = (bool)XY_Locations[i, 4]
-                    };
-                    if (method == 1 || (method == 4 && area.Has_Hordes))
+                        area = new Location
+                        {
+                            Name = string.Copy(Horde_Locations[i, 0].ToString()),
+                            Bag_Advances = Convert.ToByte(Horde_Locations[i, 1])
+                        };
                         Areas.Add(area);
+                    }
+                }
+                else
+                {
+                    for (byte i = 0; i < 23; i++)
+                    {
+                        area = new Location
+                        {
+                            Name = string.Copy(XY_Locations[i, 0].ToString()),
+                            NPC = Convert.ToByte(XY_Locations[i, 1]),
+                            Has_Hordes = (bool)XY_Locations[i, 2],
+                            ratio = Convert.ToByte(XY_Locations[i, 3]),
+                            Tall_Grass = (bool)XY_Locations[i, 4]
+                        };
+                        if (method == 1 || (method == 4 && area.Has_Hordes))
+                            Areas.Add(area);
+                    }
                 }
             }
             return Areas;
